918532f3625cbd82b4a0073ebe706cca50a8f0b3
weihsun <johnson823423@gmail.com>
2022-02-13 18:52:27 +0800

    handle input flow

--- src/componentss/common/Input/index.js
+++ src/componentss/common/Input/index.js
@@ -3,14 +3,21 @@ import style from './style.module.scss';
 const Input = ({
   placeholder = '',
   name = '',
-  value = ''
+  value = '',
+  onChange = () => {}
 }) => {
+
+  const handleOnChange = e => {
+    onChange(e.target.value);
+  }
+
   return (
     <input
       className={style.container}
       name={name}
       value={value}
       placeholder={placeholder}
+      onChange={handleOnChange}
     />
   )
 };
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,10 +1,22 @@
+import { useState, useEffect } from 'react';
+
 import Input from 'componentss/common/Input';
 
+import style from './style.module.scss';
+
 const Landing = () => {
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    // TODO: fetch api
+  }, [name]);
+
   return (
     <div>
-      <h1>Search Cat</h1>
-      <Input placeholder='input cat name' />
+      <h1 className={style.title}>Search Cat</h1>
+      <div className={style['search-input-wrapper']}>
+        <Input placeholder='input cat name' onChange={setName} value={name} />
+      </div>
     </div>
   )
 };

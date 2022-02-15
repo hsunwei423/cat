ee30f7fae5d824d9efc24fe0063e6276a873d970
Johnson <weihsun.chen@nogle.com>
2022-02-15 19:07:49 +0800

    modify api key into interceptors

--- src/api/instance.js
+++ src/api/instance.js
@@ -3,10 +3,12 @@ import axios from 'axios';
 const BASE_URL = 'https://api.thecatapi.com/v1/';
 
 const ApiInstance = axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    'x-api-key': process.env.REACT_APP_API_KEY
-  }
+  baseURL: BASE_URL
 });
+
+ApiInstance.interceptors.request.use(config => {
+  config.headers['x-api-key'] = process.env.REACT_APP_API_KEY || ''
+  return config;
+})
 
 export default ApiInstance;

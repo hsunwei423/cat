4fa953c26bbd78080bbab2e406f962e1b7041346
Johnson <weihsun.chen@nogle.com>
2022-02-17 15:33:09 +0800

    use swr to fetch data

--- src/api/catApi.js
+++ src/api/catApi.js
@@ -1,9 +1,11 @@
-import api from "./instance";
+import api from './instance';
+
+export const BREED_SEARCH_PATH = '/breeds/search';
 
 export const getBreed = (name) => {
-  return api.get('/breeds/search', { params: { q: name }});
+  return api.get(BREED_SEARCH_PATH, { params: { q: name } });
 };
 
-export const getImage = imgId => {
+export const getImage = (imgId) => {
   return api.get(`/images/${imgId}`);
 };
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,81 +1,58 @@
-import { useState, useCallback, useRef } from "react";
-import { getBreed } from "api/catApi";
-import { debounce } from "lodash-es";
-import { customizeDebounce } from 'utils';
+import { useState, useCallback, useRef, useEffect } from 'react';
+import useDebounce from 'utils/hooks/useDebounce';
+import useSWR from 'swr';
+import { BREED_SEARCH_PATH } from 'api/catApi';
 
-import Layout from "components/common/Layout";
-import Input from "components/common/Input";
-import Item from "components/Item";
+import apiInstance from 'api/instance';
+
+import Layout from 'components/common/Layout';
+import Input from 'components/common/Input';
+import Item from 'components/Item';
 import Spinner from 'components/common/Spinner';
 
 import NoDataImg from 'assets/images/img-no-data.jpg';
 
-import style from "./style.module.scss";
+import style from './style.module.scss';
 
 const LOAD_MAX_IMG_NUM = 8;
 
+const fetcher = (url) => apiInstance.get(url).then((res) => res.data);
+
 const Landing = () => {
-  const [name, setName] = useState("");
+  const [name, setName] = useState('');
   const [list, setList] = useState([]);
   const [totalList, setTotalList] = useState([]);
   const [isNoData, setNoData] = useState(false);
-  const [loading, setLoading] = useState(false);
   const nameRef = useRef(name);
-  const totalListRef = useRef([]);
+  const debounceName = useDebounce(name, name.length > 3 ? 1000 : 0);
 
-  const getData = () => {
-    setLoading(true);
-    getBreed(nameRef.current)
-      .then((res) => {
-        console.log(res.data);
-        const { data } = res;
-        // totalListRef.current = data;
-        setTotalList(data);
-        if (data.length > LOAD_MAX_IMG_NUM) {
-          setList(data.slice(0, LOAD_MAX_IMG_NUM));
-        } else {
-          setList(data);
-        }
-
-        setNoData(data.length === 0);
-      })
-      .catch((err) => {
-        console.error(err);
-        setNoData(true);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
-
-  const getDataDebounce = useCallback(
-    customizeDebounce(() => {
-      getData();
-    }, 1000),
-    []
+  const { data, error } = useSWR(
+    debounceName ? `${BREED_SEARCH_PATH}?q=${debounceName}` : null,
+    fetcher
   );
 
-  const handleInputChange = (value) => {
-    const val = value || "";
-    setName(val);
-    nameRef.current = val;
-    getDataDebounce();
-    // if (val.length > 2) {
-    //   getDataDebounce();
-    // } else {
-    //   getData();
-    // }
-  };
+  const loading = !data && !error && debounceName;
+
+  // TODO: maybe remove useEffect
+  useEffect(() => {
+    if (!data) {
+      return;
+    }
+    setTotalList(data);
+    if (data.length > LOAD_MAX_IMG_NUM) {
+      setList(data.slice(0, LOAD_MAX_IMG_NUM));
+    } else {
+      setList(data);
+    }
+    setNoData(data.length === 0);
+  }, [data]);
 
   const handleLoadMore = () => {
     const totalSize = totalList.length;
     const currentSize = list.length;
     if (totalSize > currentSize) {
       const start = currentSize;
-      const end =
-        start + LOAD_MAX_IMG_NUM > totalSize
-          ? totalSize
-          : start + LOAD_MAX_IMG_NUM;
+      const end = start + LOAD_MAX_IMG_NUM > totalSize ? totalSize : start + LOAD_MAX_IMG_NUM;
 
       setList((prev) => [...prev, ...totalList.slice(start, end)]);
     }
@@ -84,29 +61,25 @@ const Landing = () => {
   const renderItemList = () => {
     return (
       <>
-        <div className={style["item-container"]}>
-          {
-            list.map((d) => {
-              return (
-                <Item
-                  key={d.name}
-                  imgId={d.reference_image_id}
-                  name={d.name}
-                  weight={d.weight}
-                  lifeSpan={d.life_span}
-                />
-              );
-            })
-          }
+        <div className={style['item-container']}>
+          {list.map((d) => {
+            return (
+              <Item
+                key={d.name}
+                imgId={d.reference_image_id}
+                name={d.name}
+                weight={d.weight}
+                lifeSpan={d.life_span}
+              />
+            );
+          })}
         </div>
 
-        {
-          totalList.length > list.length && (
-            <div className={style["load-more-btn"]} onClick={handleLoadMore}>
-              Load More
-            </div>
-          )
-        }
+        {totalList.length > list.length && (
+          <div className={style['load-more-btn']} onClick={handleLoadMore}>
+            Load More
+          </div>
+        )}
       </>
     );
   };
@@ -115,30 +88,19 @@ const Landing = () => {
     <Layout>
       <div className={style.container}>
         <h1 className={style.title}>Search Cat</h1>
-        <div className={style["search-input-wrapper"]}>
-          <Input
-            placeholder="input cat name"
-            onChange={handleInputChange}
-            value={name}
-          />
+        <div className={style['search-input-wrapper']}>
+          <Input placeholder="input cat name" onChange={setName} value={name} />
         </div>
 
-        {
-          loading
-            ? (
-                <div className={style['status-wrapper']}>
-                  <Spinner />
-                </div>
-              )
-            : isNoData
-              ? <img
-                  className={style['status-wrapper']}
-                  src={NoDataImg}
-                  alt="no data"
-                  width="300px"
-                />
-              : renderItemList()
-        }
+        {loading ? (
+          <div className={style['status-wrapper']}>
+            <Spinner />
+          </div>
+        ) : isNoData ? (
+          <img className={style['status-wrapper']} src={NoDataImg} alt="no data" width="300px" />
+        ) : (
+          renderItemList()
+        )}
       </div>
     </Layout>
   );

4a6f84f69e22322bfb58af2ed2cf4a4d9d6e735a
Johnson <weihsun.chen@nogle.com>
2022-02-17 18:00:19 +0800

    use fixed api key on instance headers

--- src/api/instance.js
+++ src/api/instance.js
@@ -3,12 +3,10 @@ import axios from 'axios';
 const BASE_URL = 'https://api.thecatapi.com/v1/';
 
 const ApiInstance = axios.create({
-  baseURL: BASE_URL
+  baseURL: BASE_URL,
+  headers: {
+    'x-api-key': process.env.REACT_APP_API_KEY || '',
+  },
 });
-
-ApiInstance.interceptors.request.use(config => {
-  config.headers['x-api-key'] = process.env.REACT_APP_API_KEY || ''
-  return config;
-})
 
 export default ApiInstance;
